5ab41ac1b9ae9ca53131588b6ef23175114327e9
sakib5452 <najmus191@gmail.com>
2023-03-28 00:29:36 +0600

    header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Header.css'
+import logo from '../../../src/images/Logo.svg'
 const Header = () => {
     return (
-        <div className='header'>
-
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
     );
 };
 
